9c6cb2659956d8020fbd2d55516e1de25e6aeb02
Pablo Sabater <pabloosabaterr@gmail.com>
2021-02-05 23:14:47 +0100

    feat: createServerService

--- src/services/index.ts
+++ src/services/index.ts
@@ -8,7 +8,7 @@ export class Services {
         data,
       } = await axios.get(
         `${process.env.API_URL}serversettings/${serverId}/${columnName}`,
-        { headers: { 'bot-token': 123456 } }
+        { headers: { 'bot-token': process.env.BOT_TOKEN_API } }
       );
       res = data;
     } catch (err) {
@@ -16,5 +16,19 @@ export class Services {
       res = null;
     }
     return res;
+  }
+
+  async createServer(serverName: string, serverId: string): Promise<void> {
+    try {
+      await axios.post(
+        `${process.env.API_URL}server/add`,
+        { "name": serverName, "serverId": serverId },
+        {
+          headers: { 'bot-token': process.env.BOT_TOKEN_API },
+        }
+      );
+    } catch (err) {
+      console.log(err.response.data);
+    }
   }
 }

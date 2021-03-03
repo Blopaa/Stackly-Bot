ae3c357f9b0dbf624127008c92c6169a9d7f8a0b
Pablo Sabater <pabloosabaterr@gmail.com>
2021-03-03 21:57:44 +0100

    feat: create store

---
+++ src/commands/createStore.ts
@@ -0,0 +1,25 @@
+import { Guild, GuildMember } from 'discord.js';
+import { Services } from '../services';
+import { baseCommand, command, commandParametres } from '../types/command';
+
+export default class createStore extends baseCommand implements command {
+  constructor(protected services: Services) {
+    super(services);
+  }
+  public readonly name = 'createStore';
+  public readonly description: string = 'create an store for your server';
+  public readonly authorization: string = 'mod';
+  public readonly alias = 'cs';
+
+  async on({ msg }: commandParametres) {
+    try {
+      if ((<GuildMember>msg.member).hasPermission('ADMINISTRATOR')) {
+        await this.services.createStore((<Guild>msg.guild).id);
+        msg.reply('sucesfully created');
+      }
+      return
+    } catch (err) {
+      msg.reply('alredy created');
+    }
+  }
+}
